ed76d2a3b3d8968526513e799fc7f8727a143583
choi2601 <byeonghyeonchoe55@gmail.com>
2022-03-19 15:21:03 +0900

    Add: SetContentInfoForm 초기 스타일링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from './components/common/modal/Modal';
 import ModalTemplate from './components/common/modal/modalTemplate/ModalTemplate';
 import AddImgForm from './components/common/modal/modalContent/addImgForm/AddImgForm';
 import useModal from './hooks/useModal';
+import SetContentInfoForm from './components/common/modal/modalContent/setContentInfoForm/SetContentInfoForm';
 
 const App: React.FC = () => {
   const { isShowing, modalInfo, setModalVisible } = useModal();
@@ -12,7 +13,7 @@ const App: React.FC = () => {
     <button onClick={setModalVisible}>show modal</button>
     <Modal isShowing={isShowing} hide={setModalVisible}>
       <ModalTemplate>
-      <AddImgForm />
+      <SetContentInfoForm />
     </ModalTemplate>
     </Modal>
     </>
