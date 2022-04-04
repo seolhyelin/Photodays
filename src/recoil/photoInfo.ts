5b10153304363e248067761562f13aab343074a2
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-04 16:34:31 +0900

    Modify: recoil 세팅 수정

--- src/recoil/photoInfo.ts
+++ src/recoil/photoInfo.ts
@@ -1,25 +1,28 @@
 import { atom } from 'recoil';
 
-export interface PhotoInfoType {
-    id: number;
+// date => 적합 날짜에 랜더링
+export interface InfoType {
+    date: string;
     img_url: string;
     title: string;
     location: string;
     description: string;
 }
 
-export const tmpPhotoInfoState = atom<PhotoInfoType>({
-    key: 'tmpPhotoInfoState',
+export const tmpInfoState = atom<InfoType>({
+    key: 'tmpInfoState',
     default: {
-        id: 0,
-        img_url: 'empty',
+        date: 'empty', // YYYYMMDD => 20220318
+        img_url: 'empty', 
         title: 'empty',
         location: 'empty',
         description: 'empty'
     },
 })
 
-export const photoInfoListState = atom<PhotoInfoType[]>({
-    key: 'photoInfoList',
+// const [infoList, setInfoList] = useRecoilState<InfoType[]>(infoListState);
+
+export const infoListState = atom<InfoType[]>({
+    key: 'infoList',
     default: []
-})
+}) 
